5d3b3e2c5e43f658b6c5a1fb05c27e9ec0ad39bc
oleksandr.gavuka <oleksandr.gavuka@masterofcode.com>
2018-03-06 17:52:26 +0200

    fix messageHistory

--- services/agentService.js
+++ services/agentService.js
@@ -25,8 +25,10 @@ async function lastSeq(agentOptions, convId) {
     const { accountId, token } = agentOptions;
     const domain = 'va.msghist.liveperson.net';
     const messageHistory = await getConverationHistory(convId, accountId, domain, token);
-    const messages = messageHistory.conversationHistoryRecords[0].messageStatuses;
-    if (!messages && messages.length === 0) return 0;
+    const messages = messageHistory.conversationHistoryRecords &&
+    messageHistory.conversationHistoryRecords[0] &&
+    messageHistory.conversationHistoryRecords[0].messageStatuses;
+    if (!messages || messages.length === 0) return 0;
     return lodash.maxBy(messages, message => message.seq).seq;
   } catch (error) {
     log.error(error);
